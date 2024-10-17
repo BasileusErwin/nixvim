4bffbed91ebe73dca386d290665b5a080636867d
Ignacio Perez <ignacio.perez2123@protonmail.com>
2024-10-17 08:20:27 -0300

    Added custom function to get formatter for ts/js

--- config/plugins/languages/typescript.nix
+++ config/plugins/languages/typescript.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  formatters =  lib.nixvim.mkRaw ''
+    function()
+      local check_biome_if_config_exists = vim.fn.filereadable(vim.fn.getcwd() .. "/biome.json")
+      local check_biome_bin_exists = vim.fn.executable("biome")
+
+      if check_biome_if_config_exists == 1 and check_biome_bin_exists == 1 then
+        return { "biome", "prettierd", "prettier" }
+      else
+        return { "prettierd", "prettier" }
+      end
+    end
+  '';
+in
 {
   extraPackages = with pkgs; [
     prettierd
@@ -15,20 +29,8 @@
     conform-nvim = {
       enable = true;
       settings.formatters_by_ft = {
-        javascript = [
-          [
-            "biome"
-            "prettierd"
-            "prettier"
-          ]
-        ];
-        typescript = [
-          [
-            "biome"
-            "prettierd"
-            "prettier"
-          ]
-        ];
+        javascript = formatters;
+        typescript = formatters;
       };
     };
   };
